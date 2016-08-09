6eb2c003a90f37aacb0c5a91f9abac18af6d719c
Requilence <requilence@gmail.com>
2016-08-09 12:04:53 +0300

    DownloadURL error handling

--- context.go
+++ context.go
@@ -796,6 +796,9 @@ func (c *Context) DownloadURL(url string) (filePath string, err error) {
 	if err != nil {
 		return "", err
 	}
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return "", errors.New("non 2xx resp status")
+	}
 	defer resp.Body.Close()
 
 	_, err = io.Copy(out, resp.Body)
