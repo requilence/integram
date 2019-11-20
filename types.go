1cacb9a032c5f9d26902aa48bb9f8b2ac176f45a
requilence <requilence@gmail.com>
2019-11-20 17:31:25 +0200

    migrations: oauthstore

--- types.go
+++ types.go
@@ -120,6 +120,7 @@ type userProtected struct {
 	OAuthRefreshToken string
 	AuthTempToken     string // Temp token for redirect to time-limited Oauth URL to authorize the user (F.e. Trello)
 	OAuthValid    	  bool // used for stat purposes
+	OAuthStore    	  string // to detect whether non-standard store used
 
 	AfterAuthHandler string // Used to store function that will be called after successful auth. F.e. in case of interactive reply in chat for non-authed user
 	AfterAuthData    []byte // Gob encoded arg's
